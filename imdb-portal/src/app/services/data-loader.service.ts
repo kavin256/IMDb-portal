13c4cca7b9cf9659a91f06b7b10c3753a9bdbe30
kavinranawella <kavin256@gmail.com>
2021-05-30 01:04:08 +0530

    Feature: api call for movie search

---
+++ imdb-portal/src/app/services/data-loader.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataLoaderService {
+
+  constructor(
+    public http: HttpClient,
+  ) { }
+
+  // make a GET request
+  public get<T>(url: string, params: HttpParams, headers: HttpHeaders) {
+    return new Promise((resolve, reject) => {
+      this.http.get<T>(url, {
+        headers, params
+      }).subscribe(
+        ( data) => {
+          resolve(data);
+        }, (data) => {
+          reject(data);
+        });
+    });
+  }
+}
