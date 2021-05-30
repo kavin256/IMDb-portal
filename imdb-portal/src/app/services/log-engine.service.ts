2ec047d0676b8019dd6fc4f751f8818167dd2e1c
kavinranawella <kavin256@gmail.com>
2021-05-30 22:57:36 +0530

    Feature: adding logger service

---
+++ imdb-portal/src/app/services/log-engine.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogEngineService {
+
+  constructor() {
+  }
+
+  debug(message: string) {
+    const logEntry = this.createLogStatement('debug', message);
+    console.info(logEntry);
+    return logEntry;
+  }
+
+  error(message: string) {
+    const logEntry = this.createLogStatement('error', message);
+    console.error(logEntry);
+    return logEntry;
+  }
+
+  warn(message: string) {
+    const logEntry = this.createLogStatement('warning', message);
+    console.warn(logEntry);
+    return logEntry;
+  }
+
+  info(message: string) {
+    const logEntry = this.createLogStatement('info', message);
+    console.info(logEntry);
+    return logEntry;
+  }
+
+  createLogStatement(level, message) {
+    const SEPARATOR = ' ';
+    const date = this.getCurrentDate();
+    return '[' + level + ']' + SEPARATOR + date + SEPARATOR + message;
+  }
+
+  getCurrentDate() {
+    const now = new Date();
+    return '[' + now.toLocaleString() + ']';
+  }
+}
